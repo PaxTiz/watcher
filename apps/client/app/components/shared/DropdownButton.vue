0789ced644b936926c3c5713d1a27c954ab39aad
PaxTiz <vcernuta3@gmail.com>
2026-05-31 12:01:50 +0200

    fix: dropdown styles & hydratation issues

--- apps/client/app/components/shared/DropdownButton.vue
+++ apps/client/app/components/shared/DropdownButton.vue
@@ -38,12 +38,16 @@ const props = defineProps<{
 
 const isOpen = ref(false);
 
-const DropdownChild = defineComponent<{ item: (typeof props)["items"][number] }>(
-  (props) => {
-    const searchQuery = ref("");
+const DropdownChild = defineComponent(
+  (childProps: {
+    item: (typeof props)["items"][number];
+    value?: string;
+    onSelect: (key: any, value: any) => void;
+  }) => {
+    const searchQuery = ref<string | undefined>();
     const availableChildren = computed(() => {
-      let items = props.item.children ?? [];
-      if (props.item.allowSearch) {
+      let items = childProps.item.children ?? [];
+      if (childProps.item.allowSearch && searchQuery.value) {
         const query = searchQuery.value.toLowerCase();
         items = items.filter((e) => e.label.toLowerCase().includes(query));
       }
@@ -61,7 +65,7 @@ const DropdownChild = defineComponent<{ item: (typeof props)["items"][number] }>
             "bg-ui-bg border-ui-border relative z-1000 flex max-h-[200px] min-w-[var(--reka-dropdown-menu-trigger-width)] w-max flex-col overflow-y-scroll rounded border p-1 shadow shadow-ui-border dark:shadow-black",
         },
         () => [
-          props.item.allowSearch &&
+          childProps.item.allowSearch &&
             h(
               "div",
               {
@@ -75,7 +79,7 @@ const DropdownChild = defineComponent<{ item: (typeof props)["items"][number] }>
                 "onUpdate:modelValue": (v) => (searchQuery.value = v),
               }),
             ),
-          props.item.allowSearch && h("div", { class: "w-full mt-8" }),
+          childProps.item.allowSearch && h("div", { class: "w-full mt-8" }),
           ...availableChildren.value.map((child) =>
             h(
               DropdownMenuItem,
@@ -86,7 +90,7 @@ const DropdownChild = defineComponent<{ item: (typeof props)["items"][number] }>
                   (props.value === child.value
                     ? "bg-alt/10 text-alt"
                     : "text-ui-text-muted hover:text-ui-text"),
-                onClick: () => emit("select", props.item.key, child.value),
+                onClick: () => childProps.onSelect(childProps.item.key, child.value),
               },
               () => [child.icon && h(Icon, { name: child.icon }), h("span", child.label)],
             ),
@@ -94,14 +98,14 @@ const DropdownChild = defineComponent<{ item: (typeof props)["items"][number] }>
         ],
       );
   },
-  { props: ["item"] },
+  { props: ["item", "value", "onSelect"] },
 );
 </script>
 
 <template>
   <div class="relative">
     <DropdownMenuRoot v-model:open="isOpen">
-      <DropdownMenuTrigger :aria-label="label">
+      <DropdownMenuTrigger :aria-label="label" as-child>
         <Button
           :label="label"
           :icon="icon"
@@ -113,48 +117,54 @@ const DropdownChild = defineComponent<{ item: (typeof props)["items"][number] }>
       </DropdownMenuTrigger>
 
       <DropdownMenuPortal>
-        <Transition appear>
-          <DropdownMenuContent
-            :align="align ?? 'start'"
-            :side-offset="5"
-            class="bg-ui-bg border-ui-border shadow-ui-border z-1000 flex w-max min-w-[var(--reka-dropdown-menu-trigger-width)] flex-col rounded border p-1 shadow dark:shadow-black"
-          >
-            <template v-for="item in items" :key="item.label">
-              <DropdownMenuSub v-if="item.children && item.children.length > 0">
-                <DropdownMenuSubTrigger
-                  :value="item.label"
-                  as="button"
-                  class="text-ui-text hover:bg-ui-border/75 data-[state=open]:bg-ui-border/75 flex w-full cursor-pointer items-center justify-between gap-2 rounded p-1.5 text-start text-sm outline-none"
+        <ClientOnly>
+          <Transition appear>
+            <DropdownMenuContent
+              :align="align ?? 'start'"
+              :side-offset="5"
+              class="bg-ui-bg border-ui-border shadow-ui-border z-1000 flex w-max min-w-[var(--reka-dropdown-menu-trigger-width)] flex-col rounded border p-1 shadow dark:shadow-black"
+            >
+              <template v-for="item in items" :key="item.label">
+                <DropdownMenuSub v-if="item.children && item.children.length > 0">
+                  <DropdownMenuSubTrigger
+                    :value="item.label"
+                    as="button"
+                    class="text-ui-text-muted hover:hover:text-ui-text hover:bg-ui-border/75 data-[state=open]:bg-ui-border/75 flex w-full cursor-pointer items-center justify-between gap-2 rounded p-1.5 text-start text-sm outline-none"
+                  >
+                    <div class="flex items-center gap-2">
+                      <Icon v-if="item.icon" :name="item.icon" />
+                      <span>{{ item.label }}</span>
+                    </div>
+
+                    <Icon name="lucide:chevron-right" />
+                  </DropdownMenuSubTrigger>
+
+                  <DropdownMenuPortal>
+                    <DropdownChild
+                      :item="item"
+                      :value="value"
+                      :on-select="(k, v) => emit('select', k, v)"
+                    />
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+
+                <DropdownMenuItem
+                  v-else
+                  class="hover:bg-ui-border/75 data-[highlighted]:bg-ui-border/75 flex w-full cursor-pointer items-center gap-2 rounded p-1.5 text-start text-sm transition-colors outline-none"
+                  :class="
+                    value === item.value
+                      ? 'bg-alt/10 text-alt'
+                      : 'text-ui-text-muted hover:text-ui-text'
+                  "
+                  @click="emit('select', item.key, item.value as T)"
                 >
-                  <div class="flex items-center gap-2">
-                    <Icon v-if="item.icon" :name="item.icon" />
-                    <span>{{ item.label }}</span>
-                  </div>
-
-                  <Icon name="lucide:chevron-right" />
-                </DropdownMenuSubTrigger>
-
-                <DropdownMenuPortal>
-                  <DropdownChild :item="item" />
-                </DropdownMenuPortal>
-              </DropdownMenuSub>
-
-              <DropdownMenuItem
-                v-else
-                class="hover:bg-ui-border/75 data-[highlighted]:bg-ui-border/75 flex w-full cursor-pointer items-center gap-2 rounded p-1.5 text-start text-sm transition-colors outline-none"
-                :class="
-                  value === item.value
-                    ? 'bg-alt/10 text-alt'
-                    : 'text-ui-text-muted hover:text-ui-text'
-                "
-                @click="emit('select', item.key, item.value as T)"
-              >
-                <Icon v-if="item.icon" :name="item.icon" />
-                <span>{{ item.label }}</span>
-              </DropdownMenuItem>
-            </template>
-          </DropdownMenuContent>
-        </Transition>
+                  <Icon v-if="item.icon" :name="item.icon" />
+                  <span>{{ item.label }}</span>
+                </DropdownMenuItem>
+              </template>
+            </DropdownMenuContent>
+          </Transition>
+        </ClientOnly>
       </DropdownMenuPortal>
     </DropdownMenuRoot>
   </div>
